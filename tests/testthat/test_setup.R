66b3fbb47d8bc60494bf3ff3dcc5a51384b30db5
crubba <christian.rubba@googlemail.com>
2015-12-15 15:49:24 +0100

    completed first example

--- tests/testthat/test_setup.R
+++ tests/testthat/test_setup.R
@@ -1,5 +1,24 @@
-context("Inputs")
+context("tisch setup")
 
-test_that("Click is correcty executed (through click)", {
+test_that("Tisch object is setup correctly", {
+
+  data("browsershares")
+  
+  tt <- tisch(browsershares, rows = c("Year", "Month"), sep = " >> ") + 
+    theme(
+      row_style = ragged(indent = 0.5),
+      columns_style = hierarchical(),
+      column_justification = "central",
+      replace_NA = "not available",
+      text_size = "scriptsize"
+    ) + 
+    caption("Caption_Test") +
+    footnote("Footnote_Test") +
+    label("Label_Test")
+    
+  
+  expect_that(tt@annotations$caption, equals("Caption_Test"))
+  expect_that(tt@annotations$label, equals("Label_Test"))
+  expect_that(tt@annotations$footnote, equals("Footnote_Test"))
 
 })
